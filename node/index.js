cce4a67ed882079203d3acdb67947d40507a851e
writersblockchain <seanmconrad94@gmail.com>
2023-08-29 23:17:11 +0200

    nonce secretjs

---
+++ node/index.js
@@ -0,0 +1,107 @@
+import { SecretNetworkClient, Wallet } from "secretjs";
+import * as fs from "fs";
+import dotenv from "dotenv";
+import { clearLine } from "readline";
+dotenv.config();
+
+const wallet = new Wallet(process.env.MNEMONIC);
+
+const contract_wasm = fs.readFileSync("../keymap_example.wasm");
+const codeId =  933;
+const contractCodeHash =
+  "f59d55468c7b9305eb39b7a2e3663383de4033ff2819ad7959ecad859e144c78";
+const contractAddress = "secret1almvwsgfs7w0f8fvseed4y7zray3c0t9fqewvu";
+
+const secretjs = new SecretNetworkClient({
+  chainId: "pulsar-3",
+  url: "https://api.pulsar3.scrttestnet.com",
+  wallet: wallet,
+  walletAddress: wallet.address,
+});
+
+let upload_contract = async () => {
+  let tx = await secretjs.tx.compute.storeCode(
+    {
+      sender: wallet.address,
+      wasm_byte_code: contract_wasm,
+      source: "",
+      builder: "",
+    },
+    {
+      gasLimit: 4_000_000,
+    }
+  );
+
+  const codeId = Number(
+    tx.arrayLog.find((log) => log.type === "message" && log.key === "code_id")
+      .value
+  );
+
+  console.log("codeId: ", codeId);
+
+  const contractCodeHash = (
+    await secretjs.query.compute.codeHashByCodeId({ code_id: codeId })
+  ).code_hash;
+  console.log(`Contract hash: ${contractCodeHash}`);
+};
+
+// upload_contract();
+
+let instantiate_contract = async () => {
+  const initMsg = { count: 100};
+  let tx = await secretjs.tx.compute.instantiateContract(
+    {
+      code_id: codeId,
+      sender: wallet.address,
+      code_hash: contractCodeHash,
+      init_msg: initMsg,
+      label: "keymap example" + Math.ceil(Math.random() * 10000),
+    },
+    {
+      gasLimit: 400_000,
+    }
+  );
+
+  //Find the contract_address in the logs
+  const contractAddress = tx.arrayLog.find(
+    (log) => log.type === "message" && log.key === "contract_address"
+  ).value;
+
+  console.log(contractAddress);
+};
+
+// instantiate_contract();
+
+let try_increment_nonce = async () => {
+  const tx = await secretjs.tx.compute.executeContract(
+    {
+      sender: wallet.address,
+      contract_address: contractAddress,
+      msg: {
+        increment_nonce: {
+         nonce: 9
+        },
+      },
+      code_hash: contractCodeHash,
+    },
+    { gasLimit: 100_000 }
+  );
+
+  console.log(tx);
+};
+// try_increment_nonce()
+
+let query_nonce = async () => {
+  let tx = await secretjs.query.compute.queryContract({
+    contract_address: contractAddress,
+    code_hash: contractCodeHash,
+    query: {
+        get_nonce: {addr: "secret1j7n3xx4sfgjea4unghd78qvnvxdz49cxmrkqlj"
+        }
+    }});
+
+        console.log(tx)
+}
+
+
+query_nonce(); 
